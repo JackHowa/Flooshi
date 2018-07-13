9cee52ea424af6dc3b04790ac5c7a6aeb19c7cb6
jackhowa <jackhowaa@gmail.com>
2018-07-12 21:48:34 -0500

    Add play button

---
+++ src/components/OptionBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const OptionBar = () => (<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}><Icon name="ios-play" color="black" size={50} /></View>);
+
+export default OptionBar;
